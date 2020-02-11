ac449b3614bf5b447609f3c4daa12ce3b469fa04
LuciaHub <luciaprogramacion11@gmail.com>
2020-02-11 11:50:58 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ejercicios_interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loro Loros = new Loro();
+            Loros.charlar();
+        }
+    }
+        interface Icharlatan {
+            void charlar();
+        }
+    
+       abstract class Animal {
+       }
+       class Loro : Animal, Icharlatan {
+        public void charlar(){
+          Console.WriteLine("charlando");
+        }
+       }
+        class Delfin : Animal, Icharlatan {
+             public void charlar(){
+          Console.WriteLine("charlando");
+        }
+        }
+        class Tortuga : Animal {
+            public void charlar(){
+          Console.WriteLine("charlando");
+        }
+        }
+}   
